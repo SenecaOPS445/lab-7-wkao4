fd2fa38b5f613fd4fdc2f0b1cf021cc7bc3b247e
wkao4 <wkao4@myseneca.ca>
2024-07-25 00:35:24 -0400

    add lab7a.py lab7b.py lab7c.py lab7d.py lab7e.py lab7f.py lab7i.py lab7output.txt.

--- lab7b.py
+++ lab7b.py
@@ -35,6 +35,33 @@ def sum_times(t1, t2):
 
     return sum
 
+def change_time(time, seconds):
+    time.second += seconds
+    if valid_time(time) != True:
+        # Normalize seconds to minutes
+        while time.second >= 60:
+             time.second -= 60
+             time.minute +=1
+        while time.second < 0:
+             time.second += 60
+             time.minute -=1   
+        # Normalize minutes to hours
+        while time.minute >= 60:
+             time.minute -= 60
+             time.hour += 1
+        while time.minute < 0:
+             time.minute += 60
+             time.hour -= 1
+        # Hours
+        while time.hour >= 24:
+             time.hour -= 24      
+        while time.hour < 0 :
+             time.hour += 24
+
+    return None
+
+
+
 def valid_time(t):
     """check for the validity of the time object attributes:
         24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
--- lab7f.py
+++ lab7f.py
@@ -13,6 +13,16 @@ class Time:
         self.minute = minute
         self.second = second
 
+    def __str__(self):
+        '''return a string representation for the object self'''
+        return  f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'    
+    
+    def __repr__(self):
+        '''return a string representation for the object self'''
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+        return  f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'    
+
+
     def format_time(self):
         """Return time object (t) as a formatted string"""
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
@@ -23,6 +33,13 @@ class Time:
         t2_sec = t2.time_to_sec()
         sum = sec_to_time(self_sec + t2_sec)
         return sum
+    
+    def __add__(self, t2):
+       """return the result by using sum_times() method"""
+       self_sec = self.time_to_sec()
+       t2_sec = t2.time_to_sec()
+       sum = sec_to_time(self_sec + t2_sec)
+       return sum
 
     def change_time(self, seconds):
         time_seconds = self.time_to_sec()
